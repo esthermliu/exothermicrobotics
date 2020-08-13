94676f155131007a826d92a87006e129c4d0931d
Arthur Liu <artliu@cs.washington.edu>
2020-08-12 20:15:30 -0700

    Updated Alex's bio and changed the hamburger menu for phone

---
+++ _site/scripts/navigation.js
@@ -0,0 +1,72 @@
+$(document).ready(function(){
+  $("#showMinus").click(function(){ // When you click the minus sign, it will toggle actual text and the minus sign 
+    $(".TurningPoint").toggle(500);
+	$(".verticalMinus").toggle(200);
+  });
+ $("#hideMinus").click(function(){ // When you click the vertical minus sign, it will toggle actual text and the minus sign
+    $(".TurningPoint").toggle(500);
+	$(".verticalMinus").toggle(200);
+  });
+  
+ $("#Hide2").click(function(){ // When you click the words, it will toggle the actual text and the minus sign
+    $(".theZone").toggle(500);
+	$(".verticalMinus2").toggle(200);
+  });
+  $("#showMinus2").click(function(){ // When you click the minus sign, it will toggle actual text and the minus sign 
+    $(".theZone").toggle(500);
+	$(".verticalMinus2").toggle(200);
+  });
+ $("#hideMinus2").click(function(){ // When you click the vertical minus sign, it will toggle actual text and the minus sign
+    $(".theZone").toggle(500);
+	$(".verticalMinus2").toggle(200);
+  });
+  
+
+  $("#showMinus3").click(function(){ // When you click the minus sign, it will toggle actual text and the minus sign 
+    $("#phone4").toggle(500);
+	$("#hideMinus3").toggle(200);
+	$("#showMinus3").toggle(200);
+  });
+ $("#hideMinus3").click(function(){ // When you click the vertical minus sign, it will toggle actual text and the minus sign
+    $("#phone4").toggle(500);
+	$("#showMinus3").toggle(200);
+	$("#hideMinus3").toggle(200);
+  });
+  
+    
+  $("#showMinus4").click(function(){ // When you click the minus sign, it will toggle actual text and the minus sign 
+    $("#phone3").toggle(500);
+	$("#hideMinus4").toggle(200);
+	$("#showMinus4").toggle(200);
+  });
+ $("#hideMinus4").click(function(){ // When you click the vertical minus sign, it will toggle actual text and the minus sign
+    $("#phone3").toggle(500);
+	$("#showMinus4").toggle(200);
+	$("#hideMinus4").toggle(200);
+  });
+  
+
+  $("#showMinus5").click(function(){ // When you click the minus sign, it will toggle actual text and the minus sign 
+    $("#phone2").toggle(500);
+	$("#hideMinus5").toggle(200);
+	$("#showMinus5").toggle(200);
+  });
+ $("#hideMinus5").click(function(){ // When you click the vertical minus sign, it will toggle actual text and the minus sign
+    $("#phone2").toggle(500);
+	$("#showMinus5").toggle(200);
+	$("#hideMinus5").toggle(200);
+  });
+  
+
+  $("#showMinus6").click(function(){ // When you click the minus sign, it will toggle actual text and the minus sign 
+    $("#phone1").toggle(500);
+	$("#hideMinus6").toggle(200);
+	$("#showMinus6").toggle(200);
+  });
+ $("#hideMinus6").click(function(){ // When you click the vertical minus sign, it will toggle actual text and the minus sign
+    $("#phone1").toggle(500);
+	$("#showMinus6").toggle(200);
+	$("#hideMinus6").toggle(200);
+  });
+  
+});
---
+++ scripts/hamburger.js
@@ -0,0 +1,4 @@
+function menuClicked(x) {
+	x.classList.toggle("change");
+	document.getElementById("navLinks").classList.toggle("openMenu");
+}
