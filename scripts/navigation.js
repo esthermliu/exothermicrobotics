bf63379933c5a229d66eaaf07ab8d5a0727c88d1
Arthur Liu <artliu@cs.washington.edu>
2020-11-28 22:00:14 -0800

    All the stuff i had to do that ruined my break

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -46,5 +46,93 @@ $(document).ready(function(){
 	$("#plus6").toggle(200);
 	$("#minus6").toggle(200);
   });
+    
+  $("#plus7").click(function(){ // When you click the minus sign, it will toggle actual text and the minus sign 
+    $("#phone5").toggle(500);
+	$("#minus7").toggle(200);
+	$("#plus7").toggle(200);
+  });
+ $("#minus7").click(function(){ // When you click the vertical minus sign, it will toggle actual text and the minus sign
+    $("#phone5").toggle(500);
+	$("#plus7").toggle(200);
+	$("#minus7").toggle(200);
+  });
+    
+ $("#plus8").click(function(){ // When you click the minus sign, it will toggle actual text and the minus sign 
+    $("#phone6").toggle(500);
+	$("#minus8").toggle(200);
+	$("#plus8").toggle(200);
+  });
+ $("#minus8").click(function(){ // When you click the vertical minus sign, it will toggle actual text and the minus sign
+    $("#phone6").toggle(500);
+	$("#plus8").toggle(200);
+	$("#minus8").toggle(200);
+  });
+    
+ $("#plus9").click(function(){ // When you click the minus sign, it will toggle actual text and the minus sign 
+    $("#phone7").toggle(500);
+	$("#minus9").toggle(200);
+	$("#plus9").toggle(200);
+  });
+ $("#minus9").click(function(){ // When you click the vertical minus sign, it will toggle actual text and the minus sign
+    $("#phone7").toggle(500);
+	$("#plus9").toggle(200);
+	$("#minus9").toggle(200);
+  });
+    
+ $("#plus10").click(function(){ // When you click the minus sign, it will toggle actual text and the minus sign 
+    $("#phone8").toggle(500);
+	$("#minus10").toggle(200);
+	$("#plus10").toggle(200);
+  });
+ $("#minus10").click(function(){ // When you click the vertical minus sign, it will toggle actual text and the minus sign
+    $("#phone8").toggle(500);
+	$("#plus10").toggle(200);
+	$("#minus10").toggle(200);
+  });
+    
+ $("#plus11").click(function(){ // When you click the minus sign, it will toggle actual text and the minus sign 
+    $("#phone9").toggle(500);
+	$("#minus11").toggle(200);
+	$("#plus11").toggle(200);
+  });
+ $("#minus11").click(function(){ // When you click the vertical minus sign, it will toggle actual text and the minus sign
+    $("#phone9").toggle(500);
+	$("#plus11").toggle(200);
+	$("#minus11").toggle(200);
+  });
+    
+ $("#plus12").click(function(){ // When you click the minus sign, it will toggle actual text and the minus sign 
+    $("#phone10").toggle(500);
+	$("#minus12").toggle(200);
+	$("#plus12").toggle(200);
+  });
+ $("#minus12").click(function(){ // When you click the vertical minus sign, it will toggle actual text and the minus sign
+    $("#phone10").toggle(500);
+	$("#plus12").toggle(200);
+	$("#minus12").toggle(200);
+  });
+    
+ $("#plus13").click(function(){ // When you click the minus sign, it will toggle actual text and the minus sign 
+    $("#phone11").toggle(500);
+	$("#minus13").toggle(200);
+	$("#plus13").toggle(200);
+  });
+ $("#minus13").click(function(){ // When you click the vertical minus sign, it will toggle actual text and the minus sign
+    $("#phone11").toggle(500);
+	$("#plus13").toggle(200);
+	$("#minus13").toggle(200);
+  });
+    
+ $("#plus14").click(function(){ // When you click the minus sign, it will toggle actual text and the minus sign 
+    $("#phone12").toggle(500);
+	$("#minus14").toggle(200);
+	$("#plus14").toggle(200);
+  });
+ $("#minus14").click(function(){ // When you click the vertical minus sign, it will toggle actual text and the minus sign
+    $("#phone12").toggle(500);
+	$("#plus14").toggle(200);
+	$("#minus14").toggle(200);
+  });
   
-});
+}); 
